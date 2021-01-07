5b39b2a2df035819c8a60fcf4300eb7eeae5fad1
Mark Birchall <mark.birchall@gmail.com>
2021-01-07 12:16:52 +0000

    testing deploy to fbase

--- src/components/SignUp/register/Register.jsx
+++ src/components/SignUp/register/Register.jsx
@@ -29,7 +29,7 @@ export const Register = () => {
   const getUser = () => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        console.log(user)
+        //console.log(user)
         navigate("/welcome");
       } else {
         return
